b023357adeb474f380f7d6a8f52beb2f1a6b9aa1
RicardoCalvoP <a01028889@tec.mx>
2024-08-16 14:20:29 -0600

    added icons

--- test-admin2/src/App.tsx
+++ test-admin2/src/App.tsx
@@ -1,13 +1,31 @@
 import { Admin, Resource } from "react-admin";
 import { Layout } from "./Layout";
+
+// LIBRARIES DATA
+
 import { dataProvider } from "./dataProvider";
 import { authProvider } from "./authProvider";
+
+// DATA FILES
+
 import { UserList } from "./users";
 import { PostList, PostEdit, PostCreate } from "./posts";
 import { CommentCreate, CommentEdit, CommentList } from "./comments";
 import { AlbumCreate, AlbumEdit, AlbumList } from "./albums";
 import { PhotoCreate, PhotoEdit, PhotoList } from "./photos";
 import { TodosCreate, TodosEdit, TodosList } from "./todos";
+
+
+// ICONOS
+
+import PeopleIcon from '@mui/icons-material/People';
+import PostIcon from '@mui/icons-material/Description';
+import CommentIcon from '@mui/icons-material/Comment';
+import AlbumIcon from '@mui/icons-material/PhotoAlbum';
+import PhotoIcon from '@mui/icons-material/Photo';
+import TodosIcon from '@mui/icons-material/Checklist';
+
+// FUNCTIONALITY
 
 export const App = () => (
   <Admin
@@ -17,23 +35,29 @@ export const App = () => (
   >
     <Resource
       name="users"
-      list={UserList} />
+      list={UserList}
+      icon={PeopleIcon} />
 
     <Resource
       name="posts"
-      list={PostList} edit={PostEdit} create={PostCreate} />
+      list={PostList} edit={PostEdit} create={PostCreate}
+      icon={PostIcon} />
     <Resource
       name="comments"
-      list={CommentList} edit={CommentEdit} create={CommentCreate} />
+      list={CommentList} edit={CommentEdit} create={CommentCreate}
+      icon={CommentIcon} />
     <Resource
       name="albums"
-      list={AlbumList} edit={AlbumEdit} create={AlbumCreate} />
+      list={AlbumList} edit={AlbumEdit} create={AlbumCreate}
+      icon={AlbumIcon} />
     <Resource
       name="photos"
-      list={PhotoList} edit={PhotoEdit} create={PhotoCreate} />
+      list={PhotoList} edit={PhotoEdit} create={PhotoCreate}
+      icon={PhotoIcon} />
     <Resource
       name="todos"
-      list={TodosList} edit={TodosEdit} create={TodosCreate} />
+      list={TodosList} edit={TodosEdit} create={TodosCreate}
+      icon={TodosIcon} />
 
   </Admin>
 );
